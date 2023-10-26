5fe1db50595fb368fcbd47b73b0c32804466fc73
tk-yoshimura <yoshimura8192@gmail.com>
2023-10-26 23:05:48 +0900

    Initial commit

---
+++ EulerQApproximation/EulerQ.cs
@@ -0,0 +1,34 @@
+﻿using MultiPrecision;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerQApproximation {
+    public static class EulerQ<N> where N: struct, IConstant {
+        public static MultiPrecision<N> Value(MultiPrecision<N> q, long maxiter = int.MaxValue) {
+            if (!(MultiPrecision<N>.Abs(q) <= 1)) {
+                return MultiPrecision<N>.NaN;
+            }
+            if (MultiPrecision<N>.Abs(q) == 1) {
+                return 0;
+            }
+
+            MultiPrecision<N> y = 1, w = q;
+            for (long k = 1; k <= maxiter; k++) {
+                MultiPrecision<N> wm1 = 1 - w;
+
+                y *= wm1;
+                w *= q;
+
+                if (wm1 == 1) {
+                    Console.WriteLine(k);
+                    return y;
+                }
+            }
+
+            throw new ArithmeticException("Not convergence EulerQ");
+        }
+    }
+}
---
+++ EulerQApproximation/Program.cs
@@ -0,0 +1,31 @@
+﻿using MultiPrecision;
+
+namespace EulerQApproximation {
+    internal class Program {
+        static void Main() {
+            using StreamWriter sw = new("../../../../results/euler_q_n32.csv");
+            sw.WriteLine("x,y:=euler_q(x),lny:=ln(y),v:=lny*(1-x^2)");
+
+            for (MultiPrecision<Pow2.N32> x = -1; x <= 1; x += 1 / 4096d) {
+                MultiPrecision<Pow2.N32> y = EulerQ<Pow2.N32>.Value(x);
+                MultiPrecision<Pow2.N32> lny = MultiPrecision<Pow2.N32>.Log(y);
+                MultiPrecision<Pow2.N32> v = lny * (1 - x * x);
+
+                if (x == -1) {
+                    v = -MultiPrecision<Pow2.N32>.Square(MultiPrecision<Pow2.N32>.PI) / 12;
+                }
+                else if (x == 1) { 
+                    v = -MultiPrecision<Pow2.N32>.Square(MultiPrecision<Pow2.N32>.PI) / 3;
+                }
+
+                sw.WriteLine($"{x},{y},{lny},{v}");
+                Console.WriteLine($"{x}\n{y:e20}\n{lny:e20}\n{v:e20}");
+            }
+
+            sw.Close();
+
+            Console.WriteLine("END");
+            Console.Read();
+        }
+    }
+}
